71601514c74f384c964039c6dd3ce3be080ad871
= <=>
2018-09-26 14:09:37 +0200

    Show the current time on hands using fancy math

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-//These values are used just for testing
-hrPosition = 20;
-minPosition = 130;
-secPosition = 267;
-
 var date = new Date();
 console.log(date);
 
@@ -15,6 +10,10 @@ let min = date.getMinutes();
 let sec = date.getSeconds();
 
 console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
+hrPosition = (hr*360/60) + (min*(360/60)/12);
+minPosition = (min*360/60) + (sec*(360/60)/60);
+secPosition = sec*360/60;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
